76cd03052860cdb35055a4cf9891f109b18c90d9
tpo-apatheal <77015964+tpo-apatheal@users.noreply.github.com>
2021-03-02 10:24:37 -0800

    NotFound commit

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className="photo-container">
+            <img src={'./404.jpg'} alt=""/>      
+        </div>
+    );
+};
+
+export default NotFound;
